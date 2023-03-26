2e1c0040c623c7b546b3e8b53c5aa121d0775946
elizavetaosipova <evosipova_3@edu.hse.ru>
2023-03-26 17:19:22 +0700

    fixed agents

---
+++ src/main/java/restaurant/agents/TypesOfAgents.java
@@ -0,0 +1,12 @@
+package restaurant.agents;
+
+public class TypesOfAgents {
+    public static final String visitor = "visitor-agent";
+    public static final String manager = "manager-agent";
+    public static final String order = "order-agent";
+    public static final String stock = "stock-agent";
+    public static final String process = "process-agent";
+    public static final String cooker = "cooker-agent";
+    public static final String equipment = "equipment-agent";
+    public static final String menu = "menu-agent";
+}
